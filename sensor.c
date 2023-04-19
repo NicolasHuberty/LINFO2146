4c74f7a50dc5aa6b4b07022520d7c4c65bedfe25
AndrewArnita <andrew_arnita@hotmail.com>
2023-04-19 17:39:46 +0200

    sending/receiving messages + choosing parent + working on choosing parent based on RSSI

--- sensor.c
+++ sensor.c
@@ -2,27 +2,29 @@
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
 #include "net/packetbuf.h"
+#include "net/linkaddr.h"
 #include "random.h"
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h> /* For printf() */
 
 #include "sys/log.h"
-#define LOG_MODULE "Sensor Node"
+#define LOG_MODULE "Sensor"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define NUM_PARENT_CANDIDATES 3
 #define SEND_INTERVAL (8 * CLOCK_SECOND)
 
-#if MAC_CONF_WITH_TSCH
-#include "net/mac/tsch/tsch.h"
-#endif /* MAC_CONF_WITH_TSCH */
-
-static linkaddr_t parent_candidates[NUM_PARENT_CANDIDATES];
-static linkaddr_t coordinator_addr = {{ 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
-static linkaddr_t dest_addr;
-static int best_rssi = -999;
+static linkaddr_t parent_node = {{0, 0}};
 uint16_t values[3];   
 static int random_value = 0;
+
+int best_rssi = -999;
+
+struct message {
+ int rssi;
+ int nodeType;
+ int data;
+};
 
 /*---------------------------------------------------------------------------*/
 PROCESS(sensor_node_process, "Sensor Node Process");
@@ -31,89 +33,100 @@ AUTOSTART_PROCESSES(&sensor_node_process);
 /*---------------------------------------------------------------------------*/
 void input_callback(const void *data, uint16_t len,
   const linkaddr_t *src, const linkaddr_t *dest) {
-  if(len == sizeof(unsigned)) {
-    unsigned received_value;
-    memcpy(&received_value, data, sizeof(received_value));
-    LOG_INFO("Received value %u from ", received_value);
-    LOG_INFO_LLADDR(src);
-    LOG_INFO_("\n");
+  if(len == sizeof(struct message)) {
+  // Cast the message payload to a struct message pointer
+  struct message *msg = (struct message*) data;
+  printf("received %d from ", msg->data);
+  if(msg->nodeType == 0) {
+    printf("SENSOR ");
+  } else {
+    printf("COORDINATOR ");
+  } 
+  printf("node %d.%d with RSSI %d \n", src->u8[0], src->u8[1], msg->rssi);
+  
+  if(msg->data == 42){
+    parent_node = *src; // Set parent_node to current node
+    printf("chose a coordinator with address %d.%d as its parent\n", src->u8[0], src->u8[1]);
   }
+  } else {
+  printf("Invalid message length: %d (expected %d)\n", len, sizeof(struct message));
+  }
+}
+/*---------------------------------------------------------------------------*/
+void create_message(int rssi, int nodeType, int data){
+ 
+  // Allocate memory for the message
+  struct message *msg;
+  msg = (struct message*) malloc(sizeof(struct message));
+
+  //msg creation
+  msg->rssi=rssi;
+  msg->nodeType=nodeType;
+  msg->data=data;
+  
+  printf("Parent node: %d.%d\n", parent_node.u8[0], parent_node.u8[1]);
+
+  LOG_INFO("OLD rssi %d\n", best_rssi);
+  if(rssi > best_rssi){ 
+    best_rssi = rssi;
+    LOG_INFO("NEW rssi %d\n", best_rssi);
+  }
+  
+   // Send message to all nodes in the network
+  if(parent_node.u8[0] == 0) {
+    uint8_t len = sizeof(struct message);
+    void *payload = (void*)msg;
+    memcpy(nullnet_buf, payload, len);
+    nullnet_len = len;
+    NETSTACK_NETWORK.output(NULL);
+  
+    printf("Sensor Sent Message to all nodes\n");
+  }
+  // Send message only to the selected parent
+  else {
+    uint8_t len = sizeof(struct message);
+    void *payload = (void*)msg;
+    memcpy(nullnet_buf, payload, len);
+    nullnet_len = len;
+    NETSTACK_NETWORK.output(&parent_node);
+  
+    printf("Sensor Sent Message to parent node \n");
+    }
+
+  free(msg);
 }
 
 /*---------------------------------------------------------------------------*/
+
 PROCESS_THREAD(sensor_node_process, ev, data) {
 
   static struct etimer periodic_timer;
   static unsigned count = 0;
+  int rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
 
   PROCESS_BEGIN();
-
-  #if MAC_CONF_WITH_TSCH
-  tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-  #endif /* MAC_CONF_WITH_TSCH */
 
   /* Initialize NullNet */
   nullnet_buf = (uint8_t *)&random_value;
   nullnet_len = sizeof(random_value);
   nullnet_set_input_callback(input_callback);
 
-  /* Add random parent candidates to the array since we chose to go with only 3 for now */
-  linkaddr_t parent1 = {{ 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }}; // for testing I am setting this to the same addr of the coordinator
-  linkaddr_t parent2 = {{ 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
-  linkaddr_t parent3 = {{ 0x04, 0x04, 0x04, 0x04, 0x00, 0x00, 0x00, 0x00 }};
+   while(1) {
+ 
 
-  /* Add each parent candidate to the array of potential parents */
-  parent_candidates[0] = parent1;
-  parent_candidates[1] = parent2;
-  parent_candidates[2] = parent3;
+  /* Generate random sensor data */
+  for(int j = 0; j < 3; j++) {
+    values[j] = (random_rand() % 100) + 50;
+  }
 
-   while(1) {   
-    /* Select the parent among the candidates */
-    linkaddr_t best_parent = {{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
-    int rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+  /* Send random generated data */
+  random_value = values[random_rand() % 3];
+    create_message(-(rssi), 0, random_value);
 
-    for(int i = 0; i < NUM_PARENT_CANDIDATES; i++) {
-      linkaddr_t current_parent = parent_candidates[i];
-      if(linkaddr_cmp(&current_parent, &coordinator_addr)) {
-      /* Select the coordinator as the parent if it is among the candidates */
-      best_parent = current_parent;
-      break;
-      } else {
-        if(rssi >= best_rssi) {
-        LOG_INFO("Best rssi %d \n", rssi);
-          best_rssi = rssi;
-          //see why the best parent is not being taken
-          best_parent = current_parent;
-          break;
-        }
-      }
-    }
-
-    /* Generate random sensor data */
-      for(int j = 0; j < 3; j++) {
-        values[j] = (random_rand() % 100) + 50;
-      }
-
-    /* Check if the parent address is not null */
-    if(!linkaddr_cmp(&best_parent, &linkaddr_null)) {
-      /* Set the destination address to the selected parent */
-      memcpy(&dest_addr, &best_parent, sizeof(linkaddr_t));
-      LOG_INFO("Selected parent with address: ");
-      LOG_INFO_LLADDR(&dest_addr);
-      LOG_INFO_("\n");
-
-      /* Send generated random data to the parent */
-      random_value = values[random_rand() % 3];
-    LOG_INFO("Sending value %d to parent addr: ", random_value);
-      LOG_INFO_LLADDR(&dest_addr);
-      LOG_INFO_("\n");
-      
-      NETSTACK_NETWORK.output(&dest_addr);
-      count++;
-    }
+  count++;
 
     /* Wait for the next transmission interval */
-    etimer_set(&periodic_timer, SEND_INTERVAL);
+    etimer_set(&periodic_timer, SEND_INTERVAL);        
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
     etimer_reset(&periodic_timer);
   }
